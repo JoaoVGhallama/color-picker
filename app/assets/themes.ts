aba318f336766605bbf41399605489e06293c2ee
JoaoVGhallama <joao.vghallama@gmail.com>
2024-10-15 14:53:26 -0300

    added stuff

---
+++ app/assets/themes.ts
@@ -0,0 +1,10 @@
+import {DefaultTheme} from 'styled-components'
+
+export const theme: DefaultTheme = {
+    colors: {
+        primary:'#0070f3',
+        secondary: '#1c1c1ce',
+        background:'#ffffff',
+        text: '#333333'
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,62 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import './styles.css'
+
+import { useState } from "react";
+import {HexColorPicker} from "react-colorful"
+import {theme as defaultTheme} from './assets/themes'
+import {ThemeProvider, DefaultTheme} from 'styled-components'
+import {darken, lighten} from 'polished'
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [newColor, setNewColor] = useState('#aabbcc')
+  const [theme, setTheme] = useState<DefaultTheme>(defaultTheme)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleColorChange =() => {
+
+    
+    const lighterSecondary = lighten(0.3, newColor)
+    const darkerBg= darken(0.3, newColor)
+    
+    setTheme((prevTheme) => ({
+      ...prevTheme,
+      colors: {
+        ...prevTheme.colors,
+        primary: newColor, 
+        secondary: lighterSecondary,
+        background: darkerBg,
+      }
+    }))
+  }
+    
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+    <div className="container">
+      <div className="color_input">
+        <ul className="input_ul">
+          <li>
+            <h2>HEX</h2>
+            <input type="text" name="" id="" />
+          </li>
+          <li>
+            <h2>RGB</h2>
+            <input type="text" name="" id="" />
+          </li>
+          <li>
+            <h2>HSL</h2>
+            <input type="text" name="" id="" />
+          </li>
+          <li>
+            <button onClick={handleColorChange}>Mudar</button>
+            </li> 
+        </ul>
+      </div>
+    <HexColorPicker color ={newColor} onChange={setNewColor}/>
+    <div style={{color: theme.colors.primary}}>HELLO</div>
+    <div style={{color: theme.colors.secondary}}>HELLO</div>
     </div>
+    </ThemeProvider>
+    </>
   );
 }
